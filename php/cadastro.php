1c0dc589c260e3edbd89790669acd28875d19af2
CesarJulio15 <168014401+CesarJulio15@users.noreply.github.com>
2025-08-07 11:54:18 -0300

    feat: add Login.php

---
+++ php/cadastro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro | DeckLogistic</title>
+  <link rel="stylesheet" href="../css/cadastro.css">
+  <link rel="icon" href="../img/logoDecklogistic.webp" type="image/x-icon" />
+</head>
+<body>
+  <div class="container">
+    <div class="left-side">
+      
+    </div>
+    <div class="right-side">
+      <div class="form-container">
+        <img src="../img/logoDecklogistic.webp" alt="Logo" class="logo">
+        <h1>Olá Novamente!</h1>
+        <form action="processa_login.php" method="POST">
+          <input type="email" name="email" placeholder="Endereço de e-mail" required>
+          <input type="password" name="senha" placeholder="Insira sua Senha" required>
+          <div class="login-link">
+            Ainda não tem uma conta para sua empresa? <a href="../php/cadastro.php">Cadastrar</a>
+          </div>
+          <button type="submit" class="btn">Continuar</button>
+        </form>
+        <div class="divider">Ou</div>
+        <button class="google-btn">
+          <img src="../img/google.webp" alt="Google"> Continuar com o Google
+        </button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
