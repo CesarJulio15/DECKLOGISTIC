985589e2b9aaf91207490258479f55580ff42680
RafaelOliveira5501 <138042243+RafaelOliveira5501@users.noreply.github.com>
2025-08-14 12:06:52 -0300

    Commit

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,31 +1,30 @@
-<?php require_once '../config.php'; ?>
 
-<aside class="sidebar">
-<link rel="stylesheet" href="<?= ASSETS_PATH ?>/sidebar.css">
+<div class="sidebar">
+<link rel="stylesheet" href="../assets/sidebar.css">
 
   <div class="logo-area">
-    <img src="<?= IMG_PATH ?>/logoDecklogistic.webp" alt="Logo">
+    <img src="../img/logoDecklogistic.webp" alt="Logo">
   </div>
 
   <nav class="nav-section">
     <div class="nav-menus">
       <ul class="nav-list top-section">
-        <li><a href="/Pages/financeiro.php"><span><img src="<?= IMG_PATH ?>/icon-finan.svg" alt="Financeiro"></span> Financeiro</a></li>
-        <li class="active"><a href="/Pages/estoque.php"><span><img src="<?= IMG_PATH ?>/icon-estoque.svg" alt="Estoque"></span> Estoque</a></li>
+        <li><a href="/Pages/financeiro.php"><span><img src="../img/icon-finan.svg" alt="Financeiro"></span> Financeiro</a></li>
+        <li class="active"><a href="/Pages/estoque.php"><span><img src="../img/icon-estoque.svg" alt="Estoque"></span> Estoque</a></li>
       </ul>
 
       <hr>
 
       <ul class="nav-list middle-section">
-        <li><a href="/Pages/visaoGeral.php"><span><img src="<?= IMG_PATH ?>/icon-visao.svg" alt="Visão Geral"></span> Visão Geral</a></li>
-        <li><a href="/Pages/operacoes.php"><span><img src="<?= IMG_PATH ?>/icon-operacoes.svg" alt="Operações"></span> Operações</a></li>
-        <li><a href="/Pages/produtos.php"><span><img src="<?= IMG_PATH ?> /icon-produtos.svg" alt="Produtos"></span> Produtos</a></li>
+        <li><a href="/Pages/visaoGeral.php"><span><img src="../img/icon-visao.svg" alt="Visão Geral"></span> Visão Geral</a></li>
+        <li><a href="/Pages/operacoes.php"><span><img src="../img/icon-operacoes.svg" alt="Operações"></span> Operações</a></li>
+        <li><a href="/Pages/produtos.php"><span><img src="../img/icon-produtos.svg" alt="Produtos"></span> Produtos</a></li>
       </ul>
     </div>
 
     <div class="bottom-links">
-      <a href="/Pages/conta.php"><span><img src="<?= IMG_PATH ?>/icon-config.svg" alt="Conta"></span> Conta</a>
-      <a href="/Pages/dicas.php"><span><img src="<?= IMG_PATH ?>/icon-dicas.svg" alt="Dicas"></span> Dicas</a>
+      <a href="/Pages/conta.php"><span><img src="../img/icon-config.svg" alt="Conta"></span> Conta</a>
+      <a href="/Pages/dicas.php"><span><img src="../img/icon-dicas.svg" alt="Dicas"></span> Dicas</a>
     </div>
   </nav>
-</aside>
+</div>
