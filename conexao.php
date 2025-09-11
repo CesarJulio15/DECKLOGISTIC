43246335d994ac20ffda2324dfefd7b5dbef62ed
GabrielPrudente10 <168013736+GabrielPrudente10@users.noreply.github.com>
2025-09-11 16:31:19 -0300

    Update conexao.php
    Conexão com o banco de dados professor

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
     <?php
-    $conn = mysqli_connect("localhost", "root", "Home@spSENAI2025!", "decklog_db");
+    $conn = mysqli_connect("162.241.62.33", "devgom44_deckers", "deckers@1234!", "devgom44_deckers");
     if (!$conn) {
         die("Erro na conexão: " . mysqli_connect_error());
     }
