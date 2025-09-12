82875f66865bb97ca54eb2c8afbb6673e4371076
RafaelOliveira5501 <138042243+RafaelOliveira5501@users.noreply.github.com>
2025-09-12 13:34:34 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,10 @@
     <?php
-    $conn = mysqli_connect("162.241.62.33", "devgom44_deckers", "deckers@1234!", "devgom44_deckers");
+    $conn = mysqli_connect("localhost", "root", "Home@spSENAI2025!", "decklog_db");
     if (!$conn) {
         die("Erro na conexão: " . mysqli_connect_error());
     }
+    // <?php
+    // $conn = mysqli_connect("162.241.62.33", "devgom44_deckers", "deckers@1234!", "devgom44_deckers");
+    // if (!$conn) {
+    //     die("Erro na conexão: " . mysqli_connect_error());
+    // }
