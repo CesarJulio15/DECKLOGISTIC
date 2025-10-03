c67b5c29558a2e082c09e9e2c8bee87b5ef8f24f
CesarJulio15 <168014401+CesarJulio15@users.noreply.github.com>
2025-10-03 10:44:39 -0300

    update remove ctrl + L

--- Pages/auth/funcionarios/cadastrolojafuncionario.php
+++ Pages/auth/funcionarios/cadastrolojafuncionario.php
@@ -54,7 +54,7 @@ if ($nome && $email && $senha && $senha2) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cadastro de Funcionário | DeckLogistic</title>
   <link rel="stylesheet" href="../../../assets/cadastrofuncionario.css">
-  <link rel="icon" href="../../img/logoDecklogistic.webp" type="image/x-icon" />
+  <link rel="icon" href="../../../img/logoDecklogistic.webp" type="image/x-icon" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <style>
     .input-container {
