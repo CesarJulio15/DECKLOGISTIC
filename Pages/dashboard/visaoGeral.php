1ef66ebfcc776a1b8d5af2d5eaff55a9284dc5eb
GuilhermeoAparecido <168013930+GuilhermeoAparecido@users.noreply.github.com>
2025-10-17 14:21:21 -0300

    Revert "ojo"
    This reverts commit d88d17dec60f735b132e1c7bfbca695de1112672.

--- Pages/dashboard/visaoGeral.php
+++ Pages/dashboard/visaoGeral.php
@@ -58,8 +58,7 @@ $lojaId = $_SESSION['loja_id'];
           <li class="active"><a href="visaoGeral.php"><span><img src="../../img/icon-visao.svg" alt="Visão Geral"></span> Visão Geral</a></li>
 
           <li><a href="../dashboard/tabelas/produtos.php"><span><img src="../../img/icon-produtos.svg" alt="Produtos"></span> Produtos</a></li>
-           <li><a href="../dashboard/operacoes.php"><span><img src="../../img/icon-operacoes.svg" alt="Operações"></span> Histórico</a></li>
-
+                    <li><a href="../dashboard/operacoes.php"><span><img src="../../img/icon-operacoes.svg" alt="Operações"></span> Histórico</a></li>
         </ul>
       </div>
       <div class="bottom-links">
