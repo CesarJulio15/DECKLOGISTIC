99472f85660cc0c376c0f9ca234b4845b69af801
GabrielPrudente10 <168013736+GabrielPrudente10@users.noreply.github.com>
2025-11-14 14:28:00 -0300

    conexão com servidor feita

--- conexao.php
+++ conexao.php
@@ -1,10 +1,11 @@
+    <!-- // 
+    // $conn = mysqli_connect("localhost", "root", "Home@spSENAI2025!", "decklog_db");
+    // if (!$conn) {
+     //    die("Erro na conexão: " . mysqli_connect_error());
+     // }
     <?php
-    $conn = mysqli_connect("localhost", "root", "Home@spSENAI2025!", "decklog_db");
+    $conn = mysqli_connect("162.241.62.33", "devgom44_deckers", "deckers@1234!", "devgom44_deckers");
     if (!$conn) {
         die("Erro na conexão: " . mysqli_connect_error());
-    }
-    // <?php
-    // $conn = mysqli_connect("162.241.62.33", "devgom44_deckers", "deckers@1234!", "devgom44_deckers");
-    // if (!$conn) {
-    //     die("Erro na conexão: " . mysqli_connect_error());
-    // }
+    } 
+    ?>
